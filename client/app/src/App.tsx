be676570f4a76d626fed732e9cff9e9ab73f08bf
thokjend <thokjend@online.no>
2024-11-11 15:41:45 +0100

    *

--- client/app/src/App.tsx
+++ client/app/src/App.tsx
@@ -1,11 +1,34 @@
+import { useState } from "react";
+
 function App() {
+  const [ammount, setAmmount] = useState(Number);
+  const [convertFrom, setConvertFrom] = useState("");
+  const [convertTo, setConvertTo] = useState("");
+
+  const convert = async () => {
+    const response =
+      await `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${convertFrom}.json`;
+  };
+
   return (
     <>
       <div>
-        <input type="text" placeholder="ammount" />
-        <input type="text" placeholder="From" />
-        <input type="text" placeholder="To" />
-        <button>Convert</button>
+        <input
+          type="number"
+          placeholder="ammount"
+          onChange={(e) => setAmmount(Number(e.target.value))}
+        />
+        <input
+          type="text"
+          placeholder="From"
+          onChange={(e) => setConvertFrom(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="To"
+          onChange={(e) => setConvertTo(e.target.value)}
+        />
+        <button onClick={() => convert()}>Convert</button>
       </div>
     </>
   );
--- server/project/api/models.py
+++ server/project/api/models.py
@@ -4,3 +4,7 @@ class Currency(models.model):
     ammount = models.IntegerField()
     conver_from = models.models.CharField(max_length=50)
     conver_to = models.models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
