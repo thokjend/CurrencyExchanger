6a718e079f25f89291c79736d30e60d0b252dc2b
thokjend <thokjend@online.no>
2024-11-11 14:52:08 +0100

    Initial commit

---
+++ client/app/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <div>
+        <input type="text" placeholder="ammount" />
+        <input type="text" placeholder="From" />
+        <input type="text" placeholder="To" />
+        <button>Convert</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ server/project/api/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Currency(models.model):
+    ammount = models.IntegerField()
+    conver_from = models.models.CharField(max_length=50)
+    conver_to = models.models.CharField(max_length=50)
