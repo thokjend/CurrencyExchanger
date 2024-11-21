784c9d671593d7f994003c79fafe538d12b62d9b
thokjend <thokjend@online.no>
2024-11-21 13:19:05 +0100

    *

--- client/app/src/components/Header.tsx
+++ client/app/src/components/Header.tsx
@@ -1,3 +1,12 @@
+import NavButton from "./NavButton";
+
 export default function Header() {
-  return;
+  return (
+    <div className="d-flex w-50 justify-content-around position-fixed top-0">
+      <NavButton></NavButton>
+      <NavButton></NavButton>
+      <NavButton></NavButton>
+      <NavButton></NavButton>
+    </div>
+  );
 }
--- client/app/src/pages/main.tsx
+++ client/app/src/pages/main.tsx
@@ -1,10 +1,17 @@
 import Account from "../components/Account";
+import NavButton from "../components/NavButton";
 
 export default function Main() {
   return (
-    <div className="bg-dark vh-100 d-flex justify-content-center align-items-center overflow-auto">
+    <div className="bg-dark vh-100 d-flex flex-column justify-content-center align-items-center overflow-auto pt-5">
       <div className=" d-flex flex-column justify-content-center align-items-center w-50">
-        <div className="text-white fs-3">Accounts</div>
+        <div className="text-white fs-3 border d-flex fs-1 fw-bolder w-75">
+          <NavButton />
+          <NavButton />
+          Accounts
+          <NavButton />
+          <NavButton />
+        </div>
         <Account />
       </div>
     </div>
