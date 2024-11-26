95fead7512525dd599c7b77a8c088feeb53564bd
thokjend <thokjend@online.no>
2024-11-26 12:57:14 +0100

    Update main.tsx

--- client/app/src/pages/main.tsx
+++ client/app/src/pages/main.tsx
@@ -1,15 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Account from "../components/Account";
 import Header from "../components/Header";
 
 export default function Main() {
-  /* const [accountInfo, setAccountInfo] = useState([]);
+  const [accountInfo, setAccountInfo] = useState([]);
 
   const getBankAccountInfo = async () => {
-    try{
-      const response = await fetch("")
+    try {
+      const response = await fetch("http://localhost:8000/account/info");
+      if (!response.ok) {
+        throw new Error("Failed to fetch bank accounts info");
+      }
+      const data = await response.json();
+      setAccountInfo(data);
+    } catch (error) {
+      console.error("Fetch error:", error);
     }
-  }; */
+  };
+
+  useEffect(() => {
+    getBankAccountInfo();
+  }, []);
 
   return (
     <div className="d-flex flex-column justify-content-center align-items-center overflow-hidden pt-3">
